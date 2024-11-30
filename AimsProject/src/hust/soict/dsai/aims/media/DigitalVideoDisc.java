614d3767d1f0c1b8882718bb787cbd1d1e4bd2f7
MinhHoang <minhhoangdayyyy07@gmail.com>
2024-11-30 11:28:56 +0700

    add method in digital video disc

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -3,13 +3,32 @@ package hust.soict.dsai.aims.media;
 public class DigitalVideoDisc extends Disc {
     private static int nbDigitalVideoDiscs = 0;
 
-
+    // Constructor 1
     public DigitalVideoDisc(String title, String category, float cost, String director, int length) {
         super(++nbDigitalVideoDiscs, title, category, cost, length, director);
     }
 
+    // Constructor 2
     public DigitalVideoDisc(String title, String category, float cost) {
-        super(++nbDigitalVideoDiscs, title, category, cost, 0, null); 
+        super(++nbDigitalVideoDiscs, title, category, cost, 0, null);
     }
 
+    public boolean matchesTitle(String title) {
+        return getTitle().toLowerCase().contains(title.toLowerCase());  // Sử dụng getter để lấy title
+    }
+
+    public boolean matchesCategory(String category) {
+        return getCategory().equalsIgnoreCase(category);  // Sử dụng getter để lấy category
+    }
+
+    public boolean matchesPrice(float minPrice, float maxPrice) {
+
+        if (minPrice > 0 && getCost() < minPrice) {  
+            return false;
+        }
+        if (maxPrice > 0 && getCost() > maxPrice) { 
+            return false;
+        }
+        return true;
+    }
 }
